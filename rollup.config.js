660081c798b93a5865d84a02d5d81ee7a9b9a7a9
saneef <hello@saneef.com>
2020-02-28 00:22:51 +0530

    chore(xstate): adds xstate
    Updates rollup.config to hand xstate

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,39 @@
-import svelte from 'rollup-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess'
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import replace from "@rollup/plugin-replace";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: "src/main.js",
 	output: {
 		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		name: "app",
+		file: "public/build/bundle.js",
 	},
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({
-				postcss: true
+				postcss: true,
 			}),
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
-			}
+				css.write("public/build/bundle.css");
+			},
+		}),
+
+		replace({
+			"process.env.NODE_ENV": !production
+				? JSON.stringify("development")
+				: JSON.stringify("production"),
 		}),
 
 		// If you have external dependencies installed from
@@ -36,7 +43,7 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ["svelte"],
 		}),
 		commonjs(),
 
@@ -46,15 +53,15 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload("public"),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
 	],
 	watch: {
-		clearScreen: false
-	}
+		clearScreen: false,
+	},
 };
 
 function serve() {
@@ -65,11 +72,11 @@ function serve() {
 			if (!started) {
 				started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
+				require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+					stdio: ["ignore", "inherit", "inherit"],
+					shell: true,
 				});
 			}
-		}
+		},
 	};
 }
