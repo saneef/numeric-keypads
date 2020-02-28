b2c2e22789c3917ee8a2334557f4276afd3b3556
saneef <hello@saneef.com>
2020-02-28 22:02:07 +0530

    fix(use-machine): error while stopping

--- src/lib/useMachine.js
+++ src/lib/useMachine.js
@@ -3,9 +3,15 @@ import { readable } from "svelte/store";
 
 export default function(machine, options) {
 	const service = interpret(machine, options);
-	const state = readable(service.initialState, set => {
-		service.onTransition(newState => set(newState)).start();
-		return service.stop;
+	const state = readable(service.initialState, function(set) {
+		service
+			.onTransition(function(newState) {
+				return set(newState);
+			})
+			.start();
+		return () => {
+			service.stop();
+		};
 	});
 
 	return [state, service.send];
