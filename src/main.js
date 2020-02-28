5c6bf5fc8d0709b4971619e1cb3424b737420600
saneef <hello@saneef.com>
2020-02-28 14:19:38 +0530

    fix(main): disable shuffle by default

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		trials: parseInt(getSearchValue("trials"), 10) || 4,
-		disableShuffle: getSearchValue("shuffle") === "0",
+		disableShuffle: getSearchValue("shuffle") !== "1",
 	},
 });
 
