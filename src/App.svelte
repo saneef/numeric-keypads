5154fd7aa9fd63549b1cca3b6e4ee9f27407aa0c
saneef <hello@saneef.com>
2020-02-28 16:47:38 +0530

    fix(app): pad safe area for iPhone X and newer phones

--- src/App.svelte
+++ src/App.svelte
@@ -39,6 +39,9 @@
 	.grid {
 		min-height: 96vh;
 		padding-top: env(safe-area-inset-top);
+		padding-bottom: env(safe-area-inset-bottom);
+		padding-left: env(safe-area-inset-left);
+		padding-right: env(safe-area-inset-right);
 		display: grid;
 		grid-template-rows: auto 1fr;
 	}
